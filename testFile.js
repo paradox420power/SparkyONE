27ea74b0912837e95a46843ff9b5cc951304a035
ascobian <31677814+ascobian@users.noreply.github.com>
2018-12-05 12:39:04 -0700

    Add files via upload

---
+++ testFile.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function main(){
+    var input =
+"\
+\n\
+\n\
+if a <= assign:  \n\
+    a *= a * a - 7 % True / False\n\
+    \n\
+    if a <= assign:\n\
+        a *= a\n\
+    else:\n\
+        b = b\n\
+else:\n\
+    b =b         = c = false; a = a==a;";
+    var lexeme;
+    
+    document.write(input + "<br><br>");
+    
+    parse_begin_program(input);
+    
+    /*while(input.length > 0){
+        lexeme = getToken(input);
+        input = input.slice(lexeme.length);
+        document.write("ID: " + lexeme.id + ", Type: " + lexeme.type + ", Length: " + lexeme.length + ", Line: " + lexeme.line_no + "<br><br>");
+    }*/
+    document.write("END_OF_FILE");
+}
